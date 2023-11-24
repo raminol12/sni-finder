932b8709a5451183f1b06f78db02fbff80843d29
Alexporter <142115305+raminol12@users.noreply.github.com>
2023-11-24 13:39:08 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ main() {
     sudo apt update
     sudo apt install git pip -y
     git clone https://github.com/raminol12/sni-finder.git
-    (cd Reality-SNI-finder && pip install -r requirements.txt) 
-    (cd Reality-SNI-finder && python3 main.py)
+    (cd sni-finder && pip install -r requirements.txt) 
+    (cd sni-finder && python3 main.py)
 }
 
 main
